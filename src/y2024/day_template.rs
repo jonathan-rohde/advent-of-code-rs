d972ba28889f6be164831c93c25c7845e06c5365
Jonathan Rohde <johnny@parallel-bits.de>
2025-08-28 10:19:43 +0200

    update template

--- src/y2024/day_template.rs
+++ src/y2024/day_template.rs
@@ -1,5 +1,10 @@
 pub fn solve() -> (String, String) {
-    (solve_part1(), solve_part2())
+    let (part1, part2) = (solve_part1(), solve_part2());
+
+    // assert_eq!(part1, "161".to_string());
+    // assert_eq!(part2, "48".to_string());
+
+    (part1, part2)
 }
 
 fn solve_part1() -> String {
