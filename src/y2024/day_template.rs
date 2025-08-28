467ecfedf1e54c2b70946033f4da468c52cdd4cf
Jonathan Rohde <johnny@parallel-bits.de>
2025-08-28 10:22:14 +0200

    update template

--- src/y2024/day_template.rs
+++ src/y2024/day_template.rs
@@ -1,3 +1,7 @@
+use crate::util::read_file;
+
+const source: &str = "input/day04_test.txt";
+
 pub fn solve() -> (String, String) {
     let (part1, part2) = (solve_part1(), solve_part2());
 
@@ -8,9 +12,11 @@ pub fn solve() -> (String, String) {
 }
 
 fn solve_part1() -> String {
+    let input = read_file(source);
     String::from("tbd")
 }
 
 fn solve_part2() -> String {
+    let input = read_file(source);
     String::from("not yet solved")
 }
